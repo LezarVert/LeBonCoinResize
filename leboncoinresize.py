961c0659ec05f12b5c0a7fcbef36eb552366157f
LezarVert <vincent.desbeaux@gmail.com>
2016-11-15 11:28:10 +0100

    Utilisation pour plusieurs fichiers jpg

--- leboncoinresize.py
+++ leboncoinresize.py
@@ -5,25 +5,31 @@
 # pylint: disable=I0011,C0103
 import shutil
 import os
+import sys
 from PIL import Image
 CONST_SIZE_FILE = 1000
-def resize_image(path):
+def resize_image(path_resize):
     '''Resize the file in parameter for "Le bon coin"
     Keyword arguments:
-    path -- path of the image file
-    size -- size of the image
+    path_resize -- path of the image file
     '''
-    img = Image.open(path)
+    img = Image.open(path_resize)
     wpercent = (CONST_SIZE_FILE/float(img.size[0]))
     hsize = int((float(img.size[1])*float(wpercent)))
     img = img.resize((CONST_SIZE_FILE, hsize), Image.ANTIALIAS)
-    img.save(path)
+    img.save(path_resize)
 
 #copie du fichier mais il faudrait le renommer de name.jpg en name-compressed.jpg
-shutil.copyfile("G:\\python.jpg", "G:\\python-compressed.jpg")
-metadata = os.stat("G:\\python-compressed.jpg")
-#os.remove("G:\\python-compressed.jpg")
-if metadata.st_size > CONST_SIZE_FILE:
-    resize_image("G:\\python-compressed.jpg")
-else:
-    print("Le fichier est de la bonne taille")
+for path in sys.argv:
+    path_explode = os.path.splitext(path)
+    if path_explode[1] != ".jpg":
+        continue
+    print(path)
+    path_temp = os.path.splitext(path)[0] + "-py.jpg"
+    shutil.copyfile(path, path_temp)
+    metadata = os.stat(path_temp)
+    if metadata.st_size > CONST_SIZE_FILE:
+        resize_image(path_temp)
+    else:
+        print("Le fichier est de la bonne taille")
+os.system("pause")
