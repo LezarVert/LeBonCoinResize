f633180a14e0c0a81e1b30f2d20086a3bf5ba48a
LezarVert <vincent.desbeaux@gmail.com>
2016-11-15 15:10:56 +0100

    - Ajout de fichier jpeg, jpg, gif, bmp, png comme fichier admissible - Non redimensionnement des fichiers déja transformé "-py" - Supression cmmentaire inutile

--- leboncoinresize.py
+++ leboncoinresize.py
@@ -2,12 +2,14 @@
  This programme resize the image file for the website "Le bon coin"
 '''
 # -*- coding: windows-1252 -*-
-# pylint: disable=I0011,C0103
 import shutil
 import os
 import sys
+import re
 from PIL import Image
 CONST_SIZE_FILE = 1000
+LIST_EXTENSION = ['.jpeg', '.jpg', '.gif', '.bmp', '.png']
+PATERN_NAME_PY = '-py$'
 def resize_image(path_resize):
     '''Resize the file in parameter for "Le bon coin"
     Keyword arguments:
@@ -16,20 +18,17 @@ def resize_image(path_resize):
     img = Image.open(path_resize)
     wpercent = (CONST_SIZE_FILE/float(img.size[0]))
     hsize = int((float(img.size[1])*float(wpercent)))
-    img = img.resize((CONST_SIZE_FILE, hsize), Image.ANTIALIAS)
+    img = img.resize((CONST_SIZE_FILE, hsize))
     img.save(path_resize)
 
-#copie du fichier mais il faudrait le renommer de name.jpg en name-compressed.jpg
 for path in sys.argv:
     path_explode = os.path.splitext(path)
-    if path_explode[1] != ".jpg":
+    if path_explode[1] not in LIST_EXTENSION:
         continue
-    print(path)
-    path_temp = os.path.splitext(path)[0] + "-py.jpg"
+    elif re.search(PATERN_NAME_PY, path_explode[0]):
+        continue
+    path_temp = os.path.splitext(path)[0] + "-py" + path_explode[1]
     shutil.copyfile(path, path_temp)
     metadata = os.stat(path_temp)
     if metadata.st_size > CONST_SIZE_FILE:
         resize_image(path_temp)
-    else:
-        print("Le fichier est de la bonne taille")
-os.system("pause")
