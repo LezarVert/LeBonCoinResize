286bf76a8845751e691d8ed1abe72036d28d81d8
LezarVert <vincent.desbeaux@gmail.com>
2016-11-15 16:43:15 +0100

    - Retaille fichiers image dans répertoire - Découpage en fonctions - Redimensionnement de 1000 pixel max en hauteur ou largeur

--- README.rst
+++ README.rst
@@ -1,6 +1,37 @@
 LeBonCoinResize
 ===============
 Retaille les images pour les mettre sur le bon coin
+Résolution max : 1000x1000
 
-Requiers :
+Instruction
+-----------
+Ajouter un raccourci vers ce script dans "Envoyé vers" et faite un clic droit sur un fichier image.
+Seuls les extensions suivantes sont pris en compte :
+jpg
+jpeg
+png
+bmp
+gif
+
+Requiers
+--------
+pillow
+
+LeBonCoinResize
+===============
+Resize images for put on the website "Le Bon coin"
+Resolution max : 1000x1000
+
+Instruction
+-----------
+Add a shortcut in "Send To" and do a right clic on a image file.
+Only this extensions are take :
+jpg
+jpeg
+png
+bmp
+gif
+
+Needed :
+--------
 pillow
--- leboncoinresize.py
+++ leboncoinresize.py
@@ -1,5 +1,5 @@
 '''
- This programme resize the image file for the website "Le bon coin"
+ This programme resize image files for the website "Le bon coin"
 '''
 # -*- coding: windows-1252 -*-
 import shutil
@@ -7,28 +7,63 @@ import os
 import sys
 import re
 from PIL import Image
-CONST_SIZE_FILE = 1000
+CONST_SIZE_FILE = 1000 #pixel
 LIST_EXTENSION = ['.jpeg', '.jpg', '.gif', '.bmp', '.png']
 PATERN_NAME_PY = '-py$'
-def resize_image(path_resize):
+def resize_image(_path):
     '''Resize the file in parameter for "Le bon coin"
     Keyword arguments:
-    path_resize -- path of the image file
+        _path -- path of the image file
     '''
-    img = Image.open(path_resize)
-    wpercent = (CONST_SIZE_FILE/float(img.size[0]))
-    hsize = int((float(img.size[1])*float(wpercent)))
-    img = img.resize((CONST_SIZE_FILE, hsize))
-    img.save(path_resize)
+    img = Image.open(_path)
+    if img.size[0]/img.size[1] > 1:
+        wpercent = (CONST_SIZE_FILE/float(img.size[0]))
+        hsize = int((float(img.size[1])*float(wpercent)))
+        img = img.resize((CONST_SIZE_FILE, hsize))
+    else:
+        hpercent = (CONST_SIZE_FILE/float(img.size[1]))
+        wsize = int((float(img.size[0])*float(hpercent)))
+        img = img.resize((wsize, CONST_SIZE_FILE))
+    img.save(_path)
+
+def is_image_and_not_py(_path):
+    '''Test the extension and if isn't a file already resize
+    Keyword arguments:
+        _path -- path of the file
+    '''
+    _path_explode = os.path.splitext(_path)
+    if _path_explode[1] not in LIST_EXTENSION:
+        return False
+    elif re.search(PATERN_NAME_PY, _path_explode[0]):
+        return False
+    else:
+        return True
+
+def file_size_ok(_path):
+    '''Check the size and the dimension of image before resize it
+    Keyword arguments:
+        _path -- path of the image
+    '''
+    img = Image.open(_path)
+    if img.size[0] < CONST_SIZE_FILE and img.size[1] < CONST_SIZE_FILE:
+        return False
+    else:
+        return True
+
+def test_copy_and_resize(_path):
+    '''Test, copy, and resize the image file
+    Keyword arguments:
+        _path -- path of the file
+    '''
+    if is_image_and_not_py(_path) and file_size_ok(_path):
+        path_explode = os.path.splitext(_path)
+        path_out = os.path.splitext(_path)[0] + "-py" + path_explode[1]
+        shutil.copyfile(_path, path_out)
+        resize_image(path_out)
 
 for path in sys.argv:
-    path_explode = os.path.splitext(path)
-    if path_explode[1] not in LIST_EXTENSION:
-        continue
-    elif re.search(PATERN_NAME_PY, path_explode[0]):
-        continue
-    path_temp = os.path.splitext(path)[0] + "-py" + path_explode[1]
-    shutil.copyfile(path, path_temp)
-    metadata = os.stat(path_temp)
-    if metadata.st_size > CONST_SIZE_FILE:
-        resize_image(path_temp)
+    if os.path.isdir(path):
+        for path_dir in os.listdir(path):
+            test_copy_and_resize(path + '\\' + path_dir)
+    else:
+        test_copy_and_resize(path)
